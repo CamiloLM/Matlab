789e172fe5c056700c50179769ea4277867a595b
Camilo <calondonom@unal.edu.co>
2025-06-18 15:29:34 +0000

    Test

--- hello.m
+++ hello.m
@@ -1,1 +1,12 @@
-disp("Hello World")
+% disp("Hello World")
+
+% Simple MATLAB program to calculate the area of a circle
+
+% Input radius
+radius = input('Enter the radius of the circle: ');
+
+% Calculate area
+area = pi * radius^2;
+
+% Display the result
+fprintf('The area of the circle with radius %.2f is %.2f\n', radius, area);
